ef08dcb59d5395f224bf45e8ce5e828230a8e17e
JohnWL <34081873+John-WL@users.noreply.github.com>
2021-11-27 00:04:09 -0500

    fixed block scaling problem

--- python/tk_totem_square_data.py
+++ python/tk_totem_square_data.py
@@ -1,13 +1,3 @@
-def rectangle_coordinates_from_coordinate_pair(coordinate_pair, canvas_height):
-    x = coordinate_pair[0]
-    y = coordinate_pair[1]
-    scale = 10
-    return scale * x, \
-           canvas_height - scale * y, \
-           scale * x + scale, \
-           canvas_height - (scale * y + scale)
-
-
 def rectangle_color_from_shape(shape):
     if shape == "L":
         return 'purple'
@@ -31,15 +21,23 @@ class TkTotemSquareData:
         self.canvas_height = canvas_height
         self.rectangle_ids = list()
 
+    def rectangle_coordinates_from_coordinate_pair(self, coordinate_pair, canvas_height):
+        x = coordinate_pair[0]
+        y = coordinate_pair[1]
+        return self.scale * x, \
+               canvas_height - self.scale * y, \
+               self.scale * (x + 1), \
+               canvas_height - self.scale * (y + 1)
+
     def clear_rectangles_in_canvas(self, canvas):
-        for id in self.rectangle_ids:
-            canvas.delete(id)
+        for rectangle_id in self.rectangle_ids:
+            canvas.delete(rectangle_id)
         self.rectangle_ids.clear()
 
     def create_rectangles_in_canvas(self, canvas, totem_answers):
         for totem_answer in totem_answers:
             for coordinate_pair in totem_answer.coordinates:
-                rectangle_coordinates = rectangle_coordinates_from_coordinate_pair(coordinate_pair, self.canvas_height)
+                rectangle_coordinates = self.rectangle_coordinates_from_coordinate_pair(coordinate_pair, self.canvas_height)
                 rectangle_color = rectangle_color_from_shape(totem_answer.shape)
-                id = canvas.create_rectangle(rectangle_coordinates, fill=rectangle_color)
-                self.rectangle_ids.append(id)
+                rectangle_id = canvas.create_rectangle(rectangle_coordinates, fill=rectangle_color)
+                self.rectangle_ids.append(rectangle_id)
